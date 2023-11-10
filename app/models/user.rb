f6ab5b13b1aa1b2fa64dd36afba70465fa7220a7
Balaji <imnotbalaji@gmail.com>
2023-11-10 11:49:47 -0500

    Finished backend phase 1

---
+++ app/models/user.rb
@@ -0,0 +1,72 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :bigint           not null, primary key
+#  email             :string           not null
+#  password_digest   :string           not null
+#  session_token     :string           not null
+#  display_name      :string
+#  user_status       :string
+#  profile_photo_url :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+class User < ApplicationRecord
+  has_secure_password
+
+  before_validation :ensure_session_token
+
+  validates :email, 
+    uniqueness: true, 
+    length: {in: 6..255},
+    format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :session_token,
+   presence: true, 
+   uniqueness: true
+  validates :password,
+   length: {minimum: 6}, 
+   allow_nil: true
+
+
+   # Validation Methods 
+
+   def self.find_by_credentials(email, password) 
+
+    user = User.find_by(email: email)
+
+    if user && user.authenticate(password) 
+      return user
+    else
+      return nil
+    end 
+
+   end
+
+
+
+
+  # Session Tokens
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save
+    session_token
+  end 
+
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end 
+
+  def generate_session_token 
+    loop do 
+      token = SecureRandom.urlsafe_base64
+      return token if (!User.exists?(session_token: token))
+    end 
+  end 
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+ApplicationRecord.transaction do 
+    puts "Destroying tables..."
+
+    User.destroy_all
+
+    puts "Resetting primary keys..."
+
+    ApplicationRecord.connection.reset_pk_sequence!('users')
+
+    puts "Creating Users..."
+
+    User.create!(email: "demo_user1@cack.com", password: "password")
+    User.create!(email: "demo_user2@cack.com", password: "password")
+
+    10.times do 
+
+        User.create({
+        email: Faker::Movies::HarryPotter.unique.character.gsub(/\s+/,"") + "@hogwarts.com",
+        password: "password"
+        })
+
+        
+
+    end 
+
+    puts "Done"
+
+end 
